4a34ddd96f8ca238804cb678febdeccea07641cf
Etienne Gwiavander <gwiavanderetienne2@gmail.com>
2025-05-07 07:33:53 +0100

    displaying index.html on frontend from the backend

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,10 +1,23 @@
 // The address/url of this server connected to the network is: http://localhost:5003
 
 import express from 'express'
+import path, {dirname} from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 const PORT = process.env.PORT || 5003
 
-console.log("hello world")
+// Get the file path from the url of the current module
+const __filename = fileURLToPath(import.meta.url)
+
+//Get the directory name from the file path
+const __dirname = dirname(__filename)
+
+//serving up the HTML file from the /public directory
+app.get('/', (req, res) => {
+    
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    
+})
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
