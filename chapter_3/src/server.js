e1fc761a981b9c2a6e5f7a3b2fabbc1066d98194
Etienne Gwiavander <gwiavanderetienne2@gmail.com>
2025-05-07 08:04:22 +0100

    added express.static line

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -7,17 +7,20 @@ import { fileURLToPath } from 'url'
 const app = express()
 const PORT = process.env.PORT || 5003
 
-// Get the file path from the url of the current module
+// Get the file path from the url of the current module.
 const __filename = fileURLToPath(import.meta.url)
 
-//Get the directory name from the file path
+//Get the directory name from the file path.
 const __dirname = dirname(__filename)
 
-//serving up the HTML file from the /public directory
-app.get('/', (req, res) => {
-    
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-    
+// Servers the HTML file from the /public directory.
+//Tells express to serve all files from the public folder as static assets/file. Any requests for the css files will be resolved to the public directory.
+app.use(express.static(path.join(__dirname, '../public'))) // express.static line tells our code where to find the public directory.
+
+//serving up the HTML file from the /public directory.
+// Below is the endpoint that sends back the file.
+app.get('/', (req, res) => { 
+    res.sendFile(path.join(__dirname, 'public/index.html'))   
 })
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
