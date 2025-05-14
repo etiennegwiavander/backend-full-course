1c7c0c33cd3158650a20ccbeafcae91aab36588c
Etienne Gwiavander <gwiavanderetienne2@gmail.com>
2025-05-14 05:34:02 +0100

    got all todos associated with a particular user.

---
+++ chapter_3/src/middleware/authMiddleware.js
@@ -0,0 +1,5 @@
+import jwt from 'jsonwebtoken'
+
+function authMiddleware (req, res, next){
+    
+}
--- chapter_3/src/routes/todoRoutes.js
+++ chapter_3/src/routes/todoRoutes.js
@@ -6,7 +6,9 @@ const router = express.Router()
 
 //to get all todos for loged in users
 router.get('/', (req, res) => {
-    
+    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
+    const todos = getTodos.all(req.userId)
+    res.json(todos)
 })
 
 //create new todos
